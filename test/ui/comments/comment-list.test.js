db4759246926ad73ae7cc4c73b1be5673bb1c27a
Pavol Rajzak <rapasoft@gmail.com>
2018-02-26 09:34:34 +0100

    Added tests for comment list (fixed)

--- test/ui/comments/comment-list.test.js
+++ test/ui/comments/comment-list.test.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import {shallow, mount, render} from 'enzyme';
+import {mount} from 'enzyme';
 
 import testData from '../../random-data';
 import CommentList from '../../../ui/comments/comment-list';
 
 describe('CommentList', () => {
-    let commentList;
-    const props = {comments: [testData[0], testData[1]], groupBy: {}};
+    describe('that is not grouped', () => {
+        let commentList;
+        const props = {comments: [testData[0], testData[1]], groupBy: {}};
 
-    beforeEach(() => {
-        commentList = render(<CommentList {...props} />);
-    });
+        beforeEach(() => {
+            commentList = mount(<CommentList {...props} />);
+        });
 
-    it('should render list with size of comments', () => {
-        console.log(commentList.find('.w3-panel'));
-        expect(commentList.find('.w3-panel')._root.length).toBe(props.comments.length);
+        it('should render list with size of comments', () => {
+            expect(commentList.children().length).toBe(props.comments.length);
+        });
+
+        it('should contain subject for each comment', () => {
+            const subjects = commentList.find('.subject');
+            expect(subjects.length).toBe(props.comments.length);
+            expect(subjects.at(0).text().includes(props.comments[0].subject)).toBeTruthy();
+            expect(subjects.at(1).text().includes(props.comments[1].subject)).toBeTruthy();
+        });
+
+        it('should contain subheader with time', () => {
+            const subheaders = commentList.find('.subheader');
+            expect(subheaders.length).toBe(props.comments.length);
+            expect(subheaders.at(0).text().includes(props.comments[0].updatedFormatted)).toBeTruthy();
+            expect(subheaders.at(1).text().includes(props.comments[1].updatedFormatted)).toBeTruthy();
+        });
+
     });
 });
